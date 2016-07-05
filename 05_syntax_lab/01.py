276d47c8950a4f4cabb5c56fe1f3682bfba3b0c3
nirna <nir.naor@verint.com>
2016-07-05 09:21:37 +0300

    Solution for: sol-python-verint.syntax-lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,5 +1,29 @@
-"""
-Write a program that reads 10 numbers from
-the user and prints the largest one
-"""
+#Week01_Exercise01_Assignment01
+nIterations = 10
+MaxVal = float("-inf")
+for iterID in range(1,nIterations+1):
+    print "Enter number [#%d]: " % iterID
+    MaxVal = max(MaxVal, float(raw_input()))
+print "Max value is %f" % MaxVal
 
+##### Option B #####
+#nIterations = 10
+#MaxVal = ""
+#for iterID in range(1,nIterations+1):
+#    print "Enter number [#%d]: " % iterID
+#    if MaxVal == "" :
+#        MaxVal = float(raw_input())
+#    else :
+#        MaxVal = max(MaxVal, float(raw_input()))
+#print "Max value is %f" % MaxVal
+
+##### Option A #####
+#nIterations = 10
+#iterID = 1
+#print "Enter number [#%d]: " % iterID
+#MaxVal = float(raw_input())
+#while iterID < nIterations :
+#    iterID += 1
+#    print "Enter number [#%d]: " % iterID
+#    MaxVal = max(MaxVal, float(raw_input()))
+#print "Max value is %f" % MaxVal
