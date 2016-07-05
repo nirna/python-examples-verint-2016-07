4c2d96ea8b0414edc1576371c53bf3bb330371d5
nirna <nir.naor@verint.com>
2016-07-05 10:13:52 +0300

    Solution for: sol-python-verint.syntax-lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -1,8 +1,9 @@
-"""
-Write a program that reads lines from the user
-until an empty line is inserted.
-After the user typed in an empty line,
-print all previously inserted lines in reverse
-order (from last to first)
-"""
-
+#Week01_Exercise01_Assignment04
+TextOut = ""
+print "Enter first line (press enter to quit): "
+TextIn = raw_input()
+while len(TextIn) : 
+    TextOut = (TextIn + "\n" + TextOut)
+    print "Enter next line (press enter to quit): "
+    TextIn = raw_input()
+print "Inout rows in reverse order:\n\n%s" % TextOut
