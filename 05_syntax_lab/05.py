b93374c56415f3ac77c7ce800c230c46141de0ae
nirna <nir.naor@verint.com>
2016-07-05 10:30:03 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,5 @@
-"""
-Write a program that randomizes integers in a loop
-until it finds a number that is divisable by: 7, 13 and 15
-"""
-
+#Week01_Exercise01_Assignment05
+from random import randint
+x = randint(1,1000000)
+while (x%7)+(x%13)+(x%15) : x = randint(1,1000000) # Since GCD of 7, 13, 15 is 1 you can also use x % (7 * 13 * 15)
+print "Found the number %d (%d/%d=%d; %d/%d=%d; %d/%d=%d)" % (x,x,7,x/7,x,13,x/13,x,15,x/15)
