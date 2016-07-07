534c5139bf602492d7b870a212f6d050073777c2
nirna <nir.naor@verint.com>
2016-07-07 23:46:27 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -1,4 +1,7 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times
+""" 
+Print 'Hello Python' K times based on the input argument
 """
+#Week01_Exercise02_Assignment01
+import sys
+num_of_repeats = int(sys.argv[1])
+for k in range(0,num_of_repeats) : print 'Hello Python'
