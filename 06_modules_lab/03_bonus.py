f102ffae6dafa3809fe16f6c841eb966d13cf63d
nirna <nir.naor@verint.com>
2016-07-08 22:10:37 +0300

    Solution for: sol-python-verint.modules-lab/03_bonus.py

--- 06_modules_lab/03_bonus.py
+++ 06_modules_lab/03_bonus.py
@@ -1,13 +1,24 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
-
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
-
-- Take threshold and path as command line arguments
-
-Bonus: Use argparse module to parse command line arguments
+""" 
+Delete large files (larger from 1MB by default) from specified folder (default is \) 
 """
-
+#Week01_Exercise02_Assignment03_bonus
+import os
+import sys
+import argparse
+# Read input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("root_dir", 
+                    type=str,
+                    default=os.path.abspath(os.sep),
+                    help="The root folder to search under")
+parser.add_argument("size_threshold_bytes", 
+                    type=int,
+                    default=1024*1024,
+                    help="The threshold of the file size in bytes; larger files will be removed")
+args = parser.parse_args()
+# Traverse files and delete large files
+for name in os.listdir(args.root_dir):
+    filepath = args.root_dir + os.sep + name
+    if os.path.isfile(filepath) and os.path.getsize(filepath) > args.size_threshold_bytes :
+        print "Deleting large file [%d Bytes]: %s" % (os.path.getsize(filepath), filepath)
+        os.remove(filepath)
