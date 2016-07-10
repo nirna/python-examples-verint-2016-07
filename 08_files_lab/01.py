29e89c3b4c11109ee6b67969bb7220f1400a8203
nirna <nir.naor@verint.com>
2016-07-10 22:30:51 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,8 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
-
+""" Get two file paths and append the first one to the second one """
+import os
+import sys
+(_, filepath_to_append, filepath_to_append_to) = sys.argv
+with open(filepath_to_append, "rt") as fsrc:
+    with open(filepath_to_append_to, "at") as fdst:
+        for line in fsrc:
+            fdst.write(line)
