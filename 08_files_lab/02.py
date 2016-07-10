d3fe9a37a62c241f5ea65779e8e6d9aa5ca55ce3
nirna <nir.naor@verint.com>
2016-07-10 23:10:17 +0300

    Solution for: sol-python-verint.files-lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,7 +1,12 @@
-"""
-Write a program that takes 3 names. The first two
-are existing file names and the last is a new file name
-Program should write the lines from the first two 
-files interwinded into the output file
-"""
-
+""" write interlaced lines from input files 1 & 2 into the output file 3 """
+import sys
+(_, file_input_1, file_input_2, file_output) = sys.argv
+with open(file_input_1, 'r') as fin1:
+    with open(file_input_2, 'r') as fin2:
+        with open(file_output,'w') as fout:
+            line1 = fin1.readline()
+            line2 = fin2.readline()
+            while line1 + line2 != "":
+                fout.write(line1 + line2)
+                line1 = fin1.readline()
+                line2 = fin2.readline()
