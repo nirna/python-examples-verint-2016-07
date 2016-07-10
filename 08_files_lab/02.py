290528ba2c6c71f8be7a43e57d8caa6cdfd4a441
nirna <nir.naor@verint.com>
2016-07-10 23:33:18 +0300

    Solution for: sol-python-verint.files-lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,12 +1,9 @@
 """ write interlaced lines from input files 1 & 2 into the output file 3 """
 import sys
+from itertools import izip_longest
 (_, file_input_1, file_input_2, file_output) = sys.argv
 with open(file_input_1, 'r') as fin1:
     with open(file_input_2, 'r') as fin2:
         with open(file_output,'w') as fout:
-            line1 = fin1.readline()
-            line2 = fin2.readline()
-            while line1 + line2 != "":
-                fout.write(line1 + line2)
-                line1 = fin1.readline()
-                line2 = fin2.readline()
+            for line1,line2 in izip_longest(fin1, fin2, fillvalue=""):
+                fout.write(str(line1) + str(line2))
