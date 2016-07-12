5de5a4978ce91a6981060674ce35110ecd368240
nirna <nir.naor@verint.com>
2016-07-13 00:18:09 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,10 +1,15 @@
-"""
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
-"""
+""" Return IPs from hosts file per input machine name(s) """
+import sys
+from collections import defaultdict
+# Read input args
+host_file_path, pc_names = sys.argv[1], sorted(sys.argv[2:])
+# Read hosts into a dictionary
+dict_hosts = defaultdict(str)
+with open(host_file_path,'r') as fin:
+    for line in fin: 
+        (host_name, host_ip) = line.rstrip().split("=")
+        dict_hosts[host_name] = host_ip
+
+# Print results
+max_length = max(len(pc_name) for pc_name in pc_names)
+for pc_name in pc_names: print "%-*s: %s" % (max_length, pc_name, dict_hosts[pc_name] if dict_hosts.has_key(pc_name) else 'No IP')
