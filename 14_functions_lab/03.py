15aaba24c42555556bb19e194778de5556fcdb30
nirna <nir.naor@verint.com>
2016-07-15 01:07:23 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,8 @@
-"""
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
-"""
+""" sum the 10^1 digits of all input numbers """
 
+import numbers
+
+def sum10(*nums):
+    return sum([(n / 10) % 10 for n in nums if isinstance(n, numbers.Number)])
+
+print sum10(140,220,1120)
