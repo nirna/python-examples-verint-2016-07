7ef4b44c21c9fb2b2ddd469fa1fb5aa1ca1e80d7
nirna <nir.naor@verint.com>
2016-07-15 01:07:24 +0300

    Solution for: sol-python-verint.functions-lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -1,6 +1,6 @@
-"""
-Write a function that takes minlen and
-a list of words, and returns only the words
-longer than minlen
-"""
+""" return a sub-list of words that are longer than the threshold argument """
 
+def longer_than(length_thresh, *words):
+    return [w for w in words if type(w)==str and len(w)>length_thresh]
+
+print longer_than(3, 'hit', 'me', 'baby', 'one', 'more', 'time')
