446f5776eff0f6dd54ea2eb5d024f58716f195c9
nirna <nir.naor@verint.com>
2016-07-14 10:09:19 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,10 +1,11 @@
 """ Find anagrams in word list (read from a file) """
 import sys
+from collections import defaultdict
 from collections import Counter
 # Read input args
 anagrams_file_path = sys.argv[1]
 # Read words into a dictionary holding their histogram as key
-dict_anagrams = {}
+dict_anagrams = defaultdict(list)
 with open(anagrams_file_path,'r') as fin:
     for word in fin: 
         # Generate a sorted lower case letters histogram
@@ -13,8 +14,7 @@ with open(anagrams_file_path,'r') as fin:
         dict_letters = Counter(word)
         for k in sorted(dict_letters): sorted_dict_letters += k + str(dict_letters[k])
         # Add words to dictionary with their histogram as key
-        if dict_anagrams.has_key(sorted_dict_letters): dict_anagrams[sorted_dict_letters].append(word)
-        else: dict_anagrams[sorted_dict_letters] = [word]
+        dict_anagrams[sorted_dict_letters].append(word)
 
 # Print results
 for hist in dict_anagrams: print dict_anagrams[hist]
