68c03f25160580fce00fee395d7e1ece8609a259
nirna <nir.naor@verint.com>
2016-07-15 01:07:22 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,6 @@
-"""
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
-"""
+""" Throw exception if argument 1 s not a number or argument 2 is not string """
+import numbers
 
+def myfunc(myint,mystr):
+    if not isinstance(myint, numbers.Number): raise Exception("No numeric value")
+    if type(mystr) != str: raise Exception("No string value")
