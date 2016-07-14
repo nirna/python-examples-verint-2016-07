0f263177e8ec8fa608d8f0348cfa930842784964
nirna <nir.naor@verint.com>
2016-07-15 01:07:21 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,23 @@
-"""
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
-"""
+""" func1: sum all inputs; func2: multiply all inputs """
+import numbers
 
+def mysum(*nums):
+    return sum([n for n in nums if isinstance(n, numbers.Number)])
+
+def mymul(*nums):
+    mul = 1
+    for n in nums:
+        if isinstance(n, numbers.Number): mul *= n
+    return mul
+
+# returns 60
+print mysum(10, 20, 30)
+
+# returns 30
+print mysum(10, 'foo', 'bar', 20)
+
+# returns 1
+print mymul()
+
+# returns 200
+print mymul('foo', 'bar', 10, 20)
