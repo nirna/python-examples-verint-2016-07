d8d2bf20b06c2fa64311ea0dc49f8f543319129a
nirna <nir.naor@verint.com>
2016-07-20 00:42:41 +0300

    Solution for: sol-python-verint.regexp-lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -2,3 +2,6 @@ import re
 
 def toCamelCase(underscore_line):
     return re.sub(r'_(\w)', lambda m: m.group(0)[1].upper(), underscore_line)
+
+def toUnderscoreCase(camel_line):
+    return re.sub(r'([A-Z])', lambda m: '_' + m.group(0).lower(), camel_line)
