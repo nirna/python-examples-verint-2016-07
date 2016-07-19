85a0dced8cc358c12cd7d14ebfee4e50bc2ceabd
nirna <nir.naor@verint.com>
2016-07-20 00:36:38 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -7,4 +7,5 @@ with open(cfg_file_path, 'r') as cfg_file:
     for line in cfg_file:
         res = re.search(r'^\s*'+key_in+r'\s*=\s*(.*)$', line)
         if res is not None:
-            return res.group(1)
+            print res.group(1)
+            break
