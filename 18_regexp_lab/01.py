251dadefccdcb3139a30c5bac63c869eb47400cc
nirna <nir.naor@verint.com>
2016-07-20 00:28:47 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -1,12 +1,10 @@
-"""
-Write a program that reads data
-from property files.
-Each line in the file can either be:
-    An empty line
-    A comment line (Start with #)
-    A property line (of the form key = value)
+import sys
+import re
 
-Write a program that takes a property file name and key
-as command line arguments and prints the requested value
-"""
-
+cfg_file_path = sys.argv[1]
+key_in = sys.argv[2]
+with open(cfg_file_path, 'r') as cfg_file:
+    for line in cfg_file:
+        res = re.search(r'^\s*'+key_in+r'\s*=\s*(.*)$', line)
+        if res is not None:
+            return res.group(1)
