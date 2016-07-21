c9b4b3fd30116fea51391c4c24c4719bd65f708c
nirna <nir.naor@verint.com>
2016-07-22 02:38:15 +0300

    Solution for: sol-python-verint.class-syntax-lab/02.py

--- 20_classes_lab/02.py
+++ 20_classes_lab/02.py
@@ -1,4 +1,16 @@
-"""
-Following code assumes a class named MyCounter
-Fill in the code so correct result is printed
-"""
+""" Class MyCounter: counts the numer of objects instances generated """
+
+class MyCounter(object):
+    count = 0
+
+    def __init__(self):
+        MyCounter.count += 1
+
+
+#### MAIN ####
+
+for _ in range(10):
+     c1 = MyCounter()
+
+# should print 10
+print MyCounter.count
