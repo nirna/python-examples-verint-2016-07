38af9bf04a9547608e8b39531ddc9b50a1079ca2
nirna <nir.naor@verint.com>
2016-07-28 01:04:11 +0300

    Solution for: sol-python-verint.exceptions-lab/01.py

--- 25_exceptions_lab/01.py
+++ 25_exceptions_lab/01.py
@@ -4,4 +4,12 @@ and for each number prints its square root
 If value is negative or not a number show 
 a warning and keep reading values
 """
+import math
 
+while True:
+    try:
+        n = float(raw_input("Enter a nmber: "))
+        print "The square root of %f is %f" %(n,math.sqrt(n))
+
+    except ValueError as e:
+        print "Failed to calculate square root. Error was: %s" % e.message
