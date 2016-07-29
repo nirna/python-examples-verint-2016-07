4635c692a6862154b7aa51a18d9421b084e51a36
nirna <nir.naor@verint.com>
2016-07-29 04:10:42 +0300

    Solution for: sol-python-verint.classes-final-lab/player.py

---
+++ 27_tictactoe/lib/player.py
@@ -0,0 +1,11 @@
+
+class Player:
+
+    def __init__(self, name, marker):
+        if type(name)!=str: raise ValueError('Player name must be a string')
+        self.name = name
+        if type(marker)!=str or len(marker)!=1: raise ValueError('Player marker must be a single character')
+        self.marker = marker
+
+    def __str__(self):
+        return self.marker
